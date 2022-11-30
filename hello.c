5edc70c94f4005596f85d4327429ac3d7d2b4d16
Andressa <andressahenselcardoso.gmail.com>
2022-11-29 22:37:24 -0300

    Hello world

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, word!");
+}
+
+int main(void)
+{
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n", answer);
+}
